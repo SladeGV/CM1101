04373c2da21200196d5b8bd2a2fea9a354811409
SladeGV <32681839+SladeGV@users.noreply.github.com>
2017-10-19 20:52:36 +0100

    Lab6
    Lab6

---
+++ advancing the game/test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+from map import rooms
+from player import *
+from items import *
+from gameparser import *
+from game import *
---
+++ advancing the game/test2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from map import rooms
+from player import *
+from items import *
+from gameparser import *
+from game import *
+
+
+for pos in range(len(current_room["items"])):
+    print(pos)
